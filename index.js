158cfcdc535a6390ab48aded63ad51b25f256299
SumitCMLab <sb152377@gmail.com>
2025-01-25 15:58:01 +0530

    genesis: sol copy trade bot

---
+++ index.js
@@ -0,0 +1,240 @@
+const { grpcListener, wssListener } = require("./listeners");
+const { swapRaydium } = require("./swap.js");
+const { decode } = require('bs58');
+const { Keypair, Connection } = require('@solana/web3.js');
+const solConfig = require("./configurations/config.json");
+const cluster = require('cluster');
+const os = require('os');
+const chalk = require("chalk");
+const { axiosFetchWithRetries } = require("./customFetch.js");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const solrpc = process.env.solanarpc;
+const solwss = process.env.solanawss;
+const solgrpc = "https://solana-yellowstone-grpc.publicnode.com:443";
+const solprivatekey = process.env.solanaprivatekey;
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(decode(solprivatekey)));
+
+const RETRY_ATTEMPTS = 3;
+const addresses = solConfig.addresses;
+const workerPools = new Map(); // Map to store workerId -> addresses
+
+async function createGRPCListener(connection, addresses, workerId, GRPC_URL) {
+    if (!connection) {
+        connection = new Connection(
+            solrpc,
+            {
+                commitment: "confirmed",
+                wsEndpoint: solwss,
+                disableRetryOnRateLimit: false,
+                // confirmTransactionInitialTimeout: 100000,
+                async fetch(input, init) {
+                    return await axiosFetchWithRetries(input, init, RETRY_ATTEMPTS);
+                },
+            }
+        );
+    }
+
+    let listener = new grpcListener(connection, workerId, solgrpc);
+
+    listener.listenAccountsGRPC(addresses);
+
+    listener.on("tradeparams", async (data) => {
+        try {
+            const { tokenIn, tokenOut, tradeVol, accountAddress } = data;
+            if (tokenIn && tokenOut && tradeVol) {
+                // Output is in the smallest values (decimals)
+                console.log(chalk.green(`🚀 Trade coming in! 
+                    - tokenIn: **${tokenIn}**
+                    - tokenOut: **${tokenOut}**
+                    - Trade Amount: **${tradeVol}**`));
+                const transaction = await swapRaydium(connection, data, keypair.publicKey);
+                if (!transaction)
+                    return
+                console.log(chalk.red(`🚀 Copy Trade Prepared!`));
+                console.log(transaction);
+                // Transaction simulation
+                const tx = await connection.simulateTransaction(transaction.txBuffer, { sigVerify: false });
+                console.log("swap trade simulation...:", tx);
+                // Now we can execute this trade just uncomment these
+                // transaction.txBuffer.sign([keypair]);
+                // await connection.sendTransaction(transaction.txBuffer);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+};
+
+async function createWssListener(connection, addresses, workerId) {
+    if (!connection) {
+        connection = new Connection(
+            solrpc,
+            {
+                commitment: "confirmed",
+                wsEndpoint: solwss,
+                disableRetryOnRateLimit: false,
+                // confirmTransactionInitialTimeout: 100000,
+                async fetch(input, init) {
+                    return await axiosFetchWithRetries(input, init, RETRY_ATTEMPTS);
+                },
+            }
+        );
+    }
+    let listener = new wssListener(connection, workerId, solgrpc);
+
+    let subsId = [];
+    subsId = await Promise.all(addresses.map(async (address) => {
+        const id = await listener.listenAccount(address);
+        return id;
+    }));
+
+    console.log(subsId);
+
+    // connection._rpcWebSocket.on("error", (error)=> {
+    //   console.log("websocket got an error", error, "at workerId", workerId)
+    // });
+
+    listener.on("tradeparams", async (data) => {
+        data.map(async (params) => {
+            try {
+                const { tokenIn, tokenOut, tradeVol, accountAddress } = params;
+                if (tokenIn && tokenOut && tradeVol) {
+                    // Output is in the smallest values (decimals)
+                    console.log(chalk.green(`🚀 Trade coming in! 
+                    - tokenIn: **${tokenIn}**
+                    - tokenOut: **${tokenOut}**
+                    - Trade Amount: **${tradeVol} 
+                    - Signature: ${params.sig}
+                    - Address ${accountAddress}**`));
+                    const transaction = await swapRaydium(connection, params, keypair.publicKey);
+                    if (!transaction)
+                        return
+                    console.log(chalk.red(`🚀 Copy Trade Prepared!`));
+                    console.log(transaction);
+                    // Transaction simulation
+                    const tx = await connection.simulateTransaction(transaction.txBuffer, { sigVerify: false });
+                    console.log("swap trade simulation...:", tx);
+                    // Now we can execute this trade just uncomment these // Just for simulation for production use a lot of optimizations will be done for higher landing rate
+                    // transaction.txBuffer.sign([keypair]);
+                    // await connection.sendTransaction(transaction.txBuffer);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    listener.on("brokenpipe", async (data) => {
+        console.error("restarting connection at: ", workerId, data);
+        setTimeout(async () => {
+            connection = null;
+            await listener.destroy();
+            listener = null;
+            listener = await createWssListener(connection, addresses, workerId);
+            console.error("connection restarted at: ", workerId, listener.id);
+        }, 1000);
+    });
+
+    connection._rpcWebSocket.on("close", (data) => {
+        console.log("websocket connection got closed", "at workerId", workerId);
+    });
+
+    connection._rpcWebSocket.on("open", (data) => {
+        console.log("new websocket connection established", "at workerId", workerId)
+    });
+
+    return listener;
+};
+
+
+function forkCPU(assignedAddresses) {
+    let newWorker = cluster.fork();
+
+    workerPools.set(newWorker.id, assignedAddresses);
+    // Send each worker its assigned addresses
+    newWorker.send({ addresses: assignedAddresses, workerId: newWorker.id });
+    // Log worker events
+    newWorker.on('exit', (code, signal) => {
+        console.error(`Worker ${newWorker.process.pid} exited with code ${code} and signal ${signal}`);
+        // Restart the worker with the same addresses
+        const assignedAddresses = workerPools.get(newWorker.id);
+        forkCPU(assignedAddresses);
+        console.error(`Worker ${newWorker.process.pid} restarted`);
+        // newWorker = cluster.fork();
+        // workerPools.set(newWorker.id, assignedAddresses);
+        // newWorker.send({ pools: assignedAddresses, workerId });
+    });
+};
+
+const startBot = async () => {
+    try {
+        // Working on 2 threads for 10 accounts
+        const numCPUs = 2; // Math.min(os.cpus().length - 2, addresses.length); // Limit to available account or CPU cores
+
+        if (cluster.isMaster) {
+            console.log(`Master process ${process.pid} is running`);
+
+            // Fork workers, each handling specific pools
+            for (let i = 0; i < numCPUs; i++) {
+
+                // let worker = cluster.fork();
+                const assignedAddresses = addresses.slice(i * Math.ceil(addresses.length / numCPUs), (i + 1) * Math.ceil(addresses.length / numCPUs));
+                forkCPU(assignedAddresses);
+            }
+
+
+            cluster.on('message', (worker, message) => {
+                if (message.error) {
+                    console.error(`Error from worker ${worker.process.pid}:`, message.error);
+                }
+                if (message.log) {
+                    console.log(`Log from worker ${worker.process.pid}:`, message.log);
+                }
+            });
+
+        } else {
+            // Code for each worker
+            process.on('message', async ({ addresses, workerId }) => {
+                console.log(`Worker ${process.pid} listening to accounts`, addresses);
+
+                try {
+
+                    let connection = new Connection(
+                        solrpc,
+                        {
+                            commitment: "confirmed",
+                            wsEndpoint: solwss,
+                            disableRetryOnRateLimit: false,
+                            // confirmTransactionInitialTimeout: 100000,
+                            async fetch(input, init) {
+                                return await axiosFetchWithRetries(input, init, RETRY_ATTEMPTS);
+                            }
+                        }
+                    );
+
+                    // account listeners can be created for both websockets as well as grpc
+                    // if grpc access is available than grpc can be used, however this is developed with access to a free and probably only free grpc service all that node, so not thoroghly tested for reliablity
+
+                    const listener = await createGRPCListener(connection, addresses, workerId, solgrpc);
+                    // const listener = await createWssListener(connection, addresses, workerId);
+                    
+                    // Or use raw websockets just toggle between above lines
+
+                    // Depreceated for now, could be a fallback to ^subscriptionHealth
+                    // listener.monitorSubscriptions();
+
+                } catch (error) {
+                    console.log(error);
+                    process.send({ error: `Worker ${workerId} failed: ${error.message}` });
+                }
+            });
+        }
+    } catch (error) {
+        console.error(`Failed to listen:`, error);
+    }
+};
+
+startBot().catch(console.error);
---
+++ listeners.js
@@ -0,0 +1,270 @@
+const Client = require("@triton-one/yellowstone-grpc").default;
+const { CommitmentLevel } =  require("@triton-one/yellowstone-grpc");
+const bs58 = require('bs58');
+const { PublicKey } = require('@solana/web3.js');
+const { EventEmitter } = require('events');
+const { logProcessor } = require("./processor.js");
+const redis = require('./redis');
+  
+class wssListener extends EventEmitter {
+    connection;
+    logProcessor = new logProcessor();
+    prevSlot = 0;
+    checkSlot = -1;
+    workerId;
+    id;
+    subInterval;
+    constructor(connection, workerId, grpc) {
+        super();
+        this.connection = connection;
+        this.workerId = workerId;
+        this.id = new Date();
+        this.connection.onSlotChange((slot)=>{
+            // console.log(slot.slot , this.workerId, this.id);
+            this.prevSlot = slot.slot;
+        });
+        // Listener.subInterval = subscriptionHealth();
+        this.subscriptionHealth();
+    };
+    // Listen to the statechanges
+    async listenAccount(accountAddress) {
+        return this.connection.onAccountChange(
+            new PublicKey(accountAddress),
+            async (updatedAccountInfo, slot) => {
+                try {
+                    // ignoring other state changes on the same slot as we process all changes with one event.
+                    if((await redis.get(`${accountAddress}:${slot.slot}`))){
+                        // console.log("returing form here");
+                        return;
+                    }
+                    await redis.set(`${accountAddress}:${slot.slot}`, "true");
+                    redis.expire(`${accountAddress}:${slot.slot}`,30);
+                    const processedEntry = await this.logProcessor.processEventWss(this.connection, accountAddress, slot.slot).catch((error => { 
+                        throw error;
+                    }));
+                    // console.log(processedEntry);
+                    this.emit('tradeparams', processedEntry);
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+            {
+                commitment: this.connection.commitment
+            }
+        )
+    };
+    async subscriptionHealth() {
+        this.subInterval = setInterval( async ()=>{
+            // console.log("intevral running.. ", this.workerId, this.id, this.prevSlot);
+            if( this.prevSlot === this.checkSlot){
+                console.error("pipe is broken for worker: ", this.workerId, this.id);
+                // this.connection?._rpcWebSocket?.close();
+                this.emit("brokenpipe", this.id);
+                // clearInterval(this.subInterval);
+            };
+            this.checkSlot = this.prevSlot;
+            redis.set(`slot-${this.workerId}`, this.prevSlot);
+        }, 45000);
+        // return subInterval;
+    };
+    async destroy() {
+        try{
+            if (this.subInterval) {
+                clearInterval(this.subInterval);
+                this.subInterval = null;
+                console.error("Cleaned up interval for worker:", this.workerId, this.id);
+            }
+            this.removeAllListeners();
+            console.error("Cleaned up resources for worker:", this.workerId, this.id);
+            // this.connection._rpcWebSocket.shouldReconnect = false;
+            // if (this.connection._rpcWebSocket.reconnectTimeout) {
+            //     clearTimeout(this.connection._rpcWebSocket.reconnectTimeout);
+            // }
+            this.connection._rpcWebSocket.close();
+            this.connection = null;
+        } catch(error){
+            console.error(error);
+        }
+    };
+};
+
+class grpcListener extends EventEmitter {
+    logProcessor = new logProcessor();
+    GRPC_URL = "https://solana-yellowstone-grpc.publicnode.com:443";
+    PING_INTERVAL_MS = 30000;
+    workerId;
+    id;
+    subInterval;
+    constructor(connection, workerId, grpc) {
+        super();
+        this.workerId = workerId;
+        this.id = new Date();
+        if(grpc)
+            this.GRPC_URL = grpc;
+    };
+    // Listen to transactions using yellostone geyser, highly optimal
+    async listenAccountsGRPC(accountAddresses) {
+        try {                    
+            // Open connection.
+            const client = new Client(this.GRPC_URL, "X_TOKEN", {
+                "grpc.max_receive_message_length": 1024 * 1024 * 1024, // 64MiB
+            });         
+            
+            // Subscribe for events
+            const stream = await client.subscribe();
+            
+            // Create `error` / `end` handler
+            const streamClosed = new Promise((resolve, reject) => {
+                stream.on("error", (error) => {
+                    reject(error);
+                    stream.end();
+                });
+                stream.on("end", () => {
+                    resolve();
+                });
+                stream.on("close", () => {
+                    resolve();
+                });
+            });
+            
+            // Handle updates
+            stream.on("data", async (data) => {
+                let ts = new Date();
+                if (data) {
+            
+                if(data.transaction) {
+                    const tx = data.transaction;
+                    // Convert the entire transaction object
+                    const convertedTx = this.convertBuffers(tx);
+                    // console.log(`${ts.toUTCString()}: Received update: ${JSON.stringify(convertedTx)}`, convertedTx);
+                    // Process the transaction for copy trade
+                    const tradeParams = await this.logProcessor.processEventGRPC(convertedTx);
+                    this.emit("tradeparams", tradeParams);
+                }
+            
+                else {
+                    console.log(`${ts.toUTCString()}: Received update: ${data}`);
+                }
+                // stream.end();
+                } else if (data.pong) {
+                console.log(`${ts.toUTCString()}: Processed ping response!`);
+                }
+            });
+            
+            // subscribe request.
+            const request = {
+                commitment: CommitmentLevel.PROCESSED,
+                accountsDataSlice: [],
+                ping: undefined,
+                transactions: {
+                    client: {
+                    vote: false,
+                    failed: false,
+                    accountInclude: accountAddresses,
+                    // [
+                    //     "6ortatTqeicxVw2CafNunM8mLDjjCdear78s2yK2vD2y",
+                    //     "CJPRdbnuA6GK2FA63cdsJhPkWVSqdy4Mof8HzDe7TdGP",
+                    //     "5YET3YapxD6to6rqPqTWB3R9pSbURy6yduuUtoZkzoPX"
+                    //     // "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
+                    // ],
+                    accountExclude: [],
+                    accountRequired: ["675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"], // This will filter only swap trades, currently RaydiumV4 or could add more amm keys here like orca meteora, raydiumclmm
+                    },
+                },
+                // unused arguments
+                accounts: {},
+                slots: {},
+                transactionsStatus: {},
+                entry: {},
+                blocks: {},
+                blocksMeta: {},
+            };
+            
+            // Send subscribe request
+            await new Promise((resolve, reject) => {
+                stream.write(request, (err) => {
+                if (err === null || err === undefined) {
+                    resolve();
+                } else {
+                    reject(err);
+                }
+                });
+            }).catch((reason) => {
+                console.error(reason);
+                throw reason;
+            });
+            
+            // Send pings every 5s to keep the connection open
+            const pingRequest = {
+                // Required, but unused arguments
+                accounts: {},
+                accountsDataSlice: [],
+                transactions: {},
+                blocks: {},
+                blocksMeta: {},
+                slots: {},
+                transactionsStatus: {},
+                entry: {},
+            };
+            setInterval(async () => {
+                await new Promise((resolve, reject) => {
+                stream.write(pingRequest, (err) => {
+                    if (err === null || err === undefined) {
+                    resolve();
+                    } else {
+                    reject(err);
+                    }
+                });
+                }).catch((reason) => {
+                console.error(reason);
+                throw reason;
+                });
+            }, this.PING_INTERVAL_MS);
+            
+            await streamClosed;
+        } catch(error){
+            console.log(`got error while listening.. reconnecting`);
+            // startListen();
+        };
+    };
+    // utility function to process the transaction object
+    convertBuffers(obj) {
+        if (obj === null || obj === undefined) {
+            return obj;
+        }
+        // Handle Buffer objects
+        if (obj.type === 'Buffer' && Array.isArray(obj.data)) {
+        return bs58.encode(new Uint8Array(obj.data));
+        }
+
+        // Handle arrays
+        if (Array.isArray(obj)) {
+        return obj.map(item => this.convertBuffers(item));
+        }
+    
+        // Handle objects
+        if (typeof obj === 'object') {
+            // Handle Uint8Array directly
+            if (obj instanceof Uint8Array) {
+            return bs58.encode(obj);
+        }
+  
+        const converted = {};
+        for (const [key, value] of Object.entries(obj)) {
+            // Skip certain keys that shouldn't be converted
+            if (key === 'uiAmount' || key === 'decimals' || key === 'uiAmountString') {
+                converted[key] = value;
+            } else {
+                converted[key] = this.convertBuffers(value);
+            }
+        }
+            return converted;
+        }
+        return obj; 
+    }
+};
+
+module.exports = {
+    grpcListener,
+    wssListener
+};
